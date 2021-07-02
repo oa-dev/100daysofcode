0580bb21094aa16aade6ffe0eda86278ea835465
oa-dev <arroyo.ot@gmail.com>
2021-07-02 15:03:44 +0100

    add further methods on event listeners

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -4,10 +4,43 @@ class Calculator {
     this.buttons = buttons;
 
     this.display.addEventListener("input", this.handleDisplay);
+    this.buttons.addEventListener("click", this.handleButtonClick);
+    this.sum = "";
   }
 
   handleDisplay = (e) => {
     const value = e.target.value;
+  };
+
+  setDisplay = (textContent) => {
+    this.display.textContent += textContent;
+    this.sum += textContent;
+  };
+
+  handleOperator = (operator) => {
+    const operators = {
+      ["+"]: "+",
+      ["−"]: "-",
+      ["÷"]: "/",
+      ["×"]: "*",
+    };
+    this.sum += operators[operator];
+  };
+
+  calculate = (value) => {
+    return Function(`"use strict";return ${value}`)();
+  };
+
+  handleButtonClick = (e) => {
+    for (let item of e.target.classList) {
+      if (item === "number") {
+        this.setDisplay(e.target.textContent);
+      } else if (item === "operator") {
+        this.handleOperator(e.target.textContent);
+      } else if (item === "equals") {
+        console.log(this.calculate(this.sum));
+      }
+    }
   };
 }
 
