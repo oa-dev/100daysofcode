e767f349f44b7c3bf2848b5dae7e6dd931017e96
oa-dev <arroyo.ot@gmail.com>
2021-07-05 16:20:18 +0100

    Enable the loading bar to load

---
+++ loading-bar/index.js
@@ -0,0 +1,26 @@
+const loadingBar_div = document.querySelector(".loading-bar");
+const progress_div = document.createElement("div");
+progress_div.classList.add("progress");
+loadingBar_div.insertAdjacentElement("afterend", progress_div);
+
+let width = 0;
+let percent = (width / 90) * 100;
+
+const load = () => {
+  if (width > 90) {
+    clearInterval(intervalID);
+  } else {
+    loadingBar_div.style.width = `${width}vw`;
+    percent = (width / 90) * 100;
+    progress();
+    width++;
+  }
+};
+
+const progress = () => {
+  percent < 100
+    ? (progress_div.textContent = `${Math.floor(percent)}%`)
+    : (progress_div.textContent = `Loaded`);
+};
+
+const intervalID = setInterval(load, 50);
